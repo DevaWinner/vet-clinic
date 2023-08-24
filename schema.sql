b3dbbce26048357a6be33f7633005e89d4c8bfe6
DevaWinner <winneraaniekan@gmail.com>
2023-08-24 23:53:40 +0100

    Add schema to add new columns

--- schema.sql
+++ schema.sql
@@ -59,3 +59,6 @@ ALTER TABLE animals ADD PRIMARY KEY (id);
 
 ALTER TABLE animals DROP COLUMN species;
 
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
